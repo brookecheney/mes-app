59ddf76c8ee8a57427ab3745c7e3475671489695
Brooklyne Cheney <brookecheney@hotmail.com>
2021-04-01 16:33:27 -0600

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Trucks />
+  </div>
+</template>
+
+<script>
+import Trucks from './components/Trucks.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Trucks
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
